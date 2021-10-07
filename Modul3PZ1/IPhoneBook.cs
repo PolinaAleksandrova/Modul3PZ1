1f40bec445f0a241293588ec8ef964f38ee028d2
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-10-07 20:00:37 +0300

    Add project files.

---
+++ Modul3PZ1/IPhoneBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3PZ1
+{
+    public interface IPhoneBook<T>
+    {
+        void Add(T contact);
+        T Get(string key);
+        IReadOnlyCollection<T> this[string key] { get; }
+    }
+}
